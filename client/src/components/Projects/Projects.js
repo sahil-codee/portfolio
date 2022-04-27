6a07e943589876726918ece3a2752d61ff974fbe
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-04-27 16:48:49 +0530

    Update Projects section photos and description.

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
+import react from "../../Assets/Projects/react.png";
+import card from "../../Assets/Projects/card.png";
 import airbnb from "../../Assets/Projects/airbnb.png";
 import todo from "../../Assets/Projects/todo.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import destiny from "../../Assets/Projects/destiny.png";
 import meme from "../../Assets/Projects/meme.png";
 
 function Projects() {
@@ -46,39 +46,39 @@ function Projects() {
               imgPath={airbnb}
               isBlog={false}
               title="Air-bnb Clone"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="Airbnb experience clone which displays unique interactive activities led by one-of-a-kind hosts also user can see the ratings and availability of the activities."
               link="https://github.com/sahil-codee/air-bnb"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={react}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="React-Page"
+              description="This is a static page which is created using pure css and react.js."
+              link="https://sahil-codee.github.io/react-page/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={destiny}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Destiny"
+              description="A destiny is a sign-up form UI created using React.js. User can signup  ."
+              link="https://github.com/sahil-codee/destiny"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={card}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
+              title="Digital-Card"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              link="https://github.com/sahil-codee/digital-card"
             />
           </Col>
         </Row>

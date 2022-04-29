5dd35010fc23f32ea10a0fa1b5f104a5a0213edb
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-04-30 01:44:30 +0530

    Update Resume link

--- client/src/components/Resume/ResumeNew.js
+++ client/src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/sahil-codee/portfolio/master/src/Assets/SahilResume.pdf";
+  "https://raw.githubusercontent.com/sahil-codee/portfolio/master/client/src/Assets/SahilResume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);

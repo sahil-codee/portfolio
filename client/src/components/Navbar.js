4ad67a4978ce6f2db9e0cab890d7ff715b757b66
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-05-02 16:32:02 +0530

    Change fork button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -103,7 +103,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/sahil-codee"
+                href="https://github.com/sahil-codee/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
